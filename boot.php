20fe9279a6a5557f0784cf553ac7283cfdc8dfb5
voltainc <voltaincx@gmail.com>
2022-08-26 17:47:17 +0100

    added kinship db

--- boot.php
+++ boot.php
@@ -2,20 +2,32 @@
 	
 defined( 'ABSPATH' ) or die( 'ABSPATH not defined' );
 // init DB
-		  global $wpdb;
-		  $charset_collate = $wpdb->get_charset_collate();
-		  $table_name = $wpdb->prefix . 'person';
-		  $sql = "CREATE TABLE `$table_name` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `name` varchar(100) DEFAULT NULL,
-		  `age` int(11) DEFAULT NULL,
-		  `status` bool DEFAULT NULL,
-		  PRIMARY KEY(id)
-		  ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-		  ";
-		  if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-			dbDelta($sql);
-		  }
-		
+	function crudOperationsTable()
+	{
+	  global $wpdb;
+	  $charset_collate = $wpdb->get_charset_collate();
+	  $person = $wpdb->prefix . 'person';
+	  $kinship = $wpdb->prefix . 'kinship';
+	  $sql = "CREATE TABLE `$person` (
+	  `id` int(11) NOT NULL AUTO_INCREMENT,
+	  `name` varchar(100) DEFAULT NULL,
+	  `age` int(11) DEFAULT NULL,
+	  `status` bool DEFAULT NULL,
+	  PRIMARY KEY(id)
+	  ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+	  
+	  CREATE TABLE `$kinship` (
+	  `id` int(11) NOT NULL AUTO_INCREMENT,
+	  `person` int(11) DEFAULT NULL,
+	  `kin` int(11) DEFAULT NULL,
+	  `relation` varchar(100) DEFAULT NULL,
+	  `status` bool DEFAULT NULL,
+	  PRIMARY KEY(id)
+	  ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+	  ";
+	  if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	  }
+	}
 ?>
